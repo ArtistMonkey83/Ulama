ab014b02d946822b4d46de42eded865b57ebc561
ArtistMonkey83 <29446351+ArtistMonkey83@users.noreply.github.com>
2023-06-19 21:16:15 -0700

    Counters and timers added
    Some progress on the counters and timers, everything compiles, not sure about the implementation of the game and shot clock timers. These need to be refined to ensure goals can be recorded while the timers count up to 5 minutes per player turn and a total game time of 30 minutes.

--- LED.c
+++ LED.c
@@ -2,6 +2,13 @@
 #include "SysTick.h"
 #include "tm4c123gh6pm.h"
 
+/* The array for the color for LED display
+		
+       23  22  21  20  19  18  17  16  15  14  13  12  11  10  9   8   7   6   5   4   3   2   1   0
+			 G7  G6  G5  G4  G3  G2  G1  G0  R7  R6  R5  R4  R3  R2  R1  R0  B7  B6  B5  B4  B3  B2  B1  B0 
+*/
+
+//uint32_t LEDPattern[16] = {0xFFF,0xEEE,0xDDD,0xCCC,0xBBB,0xAAA,0x999,0x888,0x777,0x666,0x555,0x444,0x333,0x222,0x111,0x000}; 
 
 /* The LEDs should flash 3 times and then stay on when Ulama is activated */
 void LEDon(){
@@ -18,7 +25,7 @@ void LEDon(){
 			GPIO_PORTB_DATA_R = 0x02;		//Turn on the red LEDs
 }
 
-/* The LEDs should flash faster 2 times, (DIM), and then stay off when Ulama is deactivated */
+/* The ON/OFF LEDs should flash faster 2 times, (DIM), and then stay off when Ulama is deactivated */
 void LEDoff(){
 			GPIO_PORTB_DATA_R = 0x00;		//Turn off the red LEDs
 			SysTick_Wait200ms(3);				// Wait
@@ -28,7 +35,16 @@ void LEDoff(){
 }
 
 void LEDgoal(){
-
+			GPIO_PORTB_DATA_R = 0x00;		//Turn off the red LEDs
+			GPIO_PORTB_DATA_R = 0x02;		//Turn on the red LEDs
+			SysTick_Wait200ms(1);				// Wait
+			GPIO_PORTB_DATA_R = 0x00;		//Turn off the red LEDs
+			SysTick_Wait200ms(1);				// Wait
+			GPIO_PORTB_DATA_R = 0x02;		//Turn on the red LEDs
+			SysTick_Wait200ms(1);				// Wait
+			GPIO_PORTB_DATA_R = 0x00;		//Turn off the red LEDs
+			SysTick_Wait200ms(1);				// Wait
+			GPIO_PORTB_DATA_R = 0x02;		//Turn on the red LEDs
 }
 
 //void LEDindoor(){}
--- main.c
+++ main.c
@@ -17,82 +17,172 @@
 #define PE 0x10		//HW Serial: Pin0 Rx[7], Pin1 Tx[7] Analog: Pin2 & Pin3 I2C: Pin4 SCL[2], Pin5 SDA[2]
 #define PF 0x20		//SPI: Pin0 MISO[1], Pin1 MOSI[1], Pin2 SCK[1], Pin3 CS[1]
 
+int goal();					//Fucntion to determine state of goals, was one made==1 or no goal==0
+int pwrDwn();				//Function to determine if the game should perform power down protocols
+int reset();				//Function to determine if reset the game
+int shotClock();		//Function to track the time remaining for a player to take a shot, 5 minute maximum
+int gameTimer();		//Function to track the overall time remaining till game is over, 30 minute maximum
+int ulamaTimeout();	//Function to automatically turn off the game after 33 minutes, 3 minutes after the end of the game.
 
-//uint32_t delay = 0;
-//int32_t i;
+void welcome();			//Function to display a welcome message
+void goodBye();			//Function to display a goodbye message
+void goalScored();	//Function to display Goal message
+void display();			//Function to display player name
+
+int SHOTCLOCK = 0;	//Global variable for how much time a player has left for their turn
+int GAMETIMER = 0;	//Global variable for how much time is left in the currrent game
+int TIMEOUT = 0;		//Global variable for the timeout signal
+int PLAYER = 0; 		//Global variable to hold the current player, 0 == player 1, 1 == player 2
+int TURNS = 0;			//Global variable to hold the number of turns taken
+
 int main(){
-		/* The array for the color for LED display
+		char player1[9] = {'P','l','a','y','e','r',' ','1','\0'}; //c-string to hold player1 name
+		char player2[9] = {'P','l','a','y','e','r',' ','2','\0'};	//c-string to hold player2 name
 		
-       23  22  21  20  19  18  17  16  15  14  13  12  11  10  9   8   7   6   5   4   3   2   1   0
-			 G7  G6  G5  G4  G3  G2  G1  G0  R7  R6  R5  R4  R3  R2  R1  R0  B7  B6  B5  B4  B3  B2  B1  B0 
-		*/
-		//uint32_t LEDPattern[16] = {0xFFF,0xEEE,0xDDD,0xCCC,0xBBB,0xAAA,0x999,0x888,0x777,0x666,0x555,0x444,0x333,0x222,0x111,0x000}; 
-		
-		//uint32_t delay = 0;
-		int32_t PwrDwn,	//Variable holding state of the game, is it on==0 or off==1?
-						goal;		//Variable holding state of goals, was one made==1 or no goal==0?
-/*Polling if the game is turned on*/
-		PwrDwn = 0;// Remove this when implementing Polling
+		int goalCounter[2] = {0, 0};		//Array to hold the number of goals made by all players
 	
-		// call the function to initialize PLL
-		PLL_Init();
-		
-		// call the function to initialize SysTick
-		SysTick_Init();
+		PLL_Init();			/* call the function to initialize PLL*/
+		SysTick_Init(); /* call the function to initialize SysTick*/
+	
 /* enable clock to GPIOA */
-		/* enable clock to GPIO B and C */
-		SYSCTL_RCGCGPIO_R = /*PA | */ PB | PC;
-		/* set PORTB pins 0 & 1 as output to drive LEDs */
-		GPIO_PORTB_DIR_R = 0x03;
-		/* set PORTB pins 0 & 1 as digital pins */
-		GPIO_PORTB_DEN_R = 0x03;
-		
-		/* set PORTC pin 4 as input (turning on switch) and 7 as output (speaker)*/
-		GPIO_PORTC_DIR_R = 0x80;
-		/* set PORTC pin 4 & 7 as digital pins */
-		GPIO_PORTC_DEN_R = 0x90;
-		
-		//while(1){
-			PwrDwn = 0; // Is there incomming signal from on/off button?
-			/* If the PwrDwn variable is == 0 the game is turned on */		
-			if(PwrDwn == 0){
-				LEDon(); //Turn on the LEDs
-		/* Display Welcome Message on screen */
-		/* Play boot up tones on speaker */	
-				SpeakerOn(); //Play the activation tone
-		/* Check if there is a goal scored */
-				if(goal == 1){
-					LEDgoal();
-					SpeakerGoal();
-				}
-		}
-			/* If the game is swtiched off  */		
-			else if (PwrDwn == 1){
-/* Call Shutdown function */
-				LEDoff();	//Turn off the LEDs when powering down
-				SpeakerOff();	//play tone when powering down
-		/* Display Welcome Message on screen */
-		/* Play boot up tones on speaker */
-		}
+/* enable clock to GPIO B and C */
+/* PortB, Pin0 == Goal, Green LEDs flash when button pushed OUTPUT*/
+/* PortB, Pin1 == On/off, Red LEDs flash when switch activated/deactivated OUTPUT*/
+/* PortC, Pin4 simulation of displacement sensor as a switch INPUT*/
+/* PortC, Pin7 controls speaker tone OUTPUT*/
 
-		//}
-/* Pin0 == Goal, Green LEDs flash when button pushed */
-
-
-     /* set PORTA pin 7 as output to drive the speaker */
-		//GPIO_PORTA_DIR_R = 0x80;
-     /* set PORTA pin 7 as a digital pin */
-		//GPIO_PORTA_DEN_R = 0x80;
-		// The code for the countdown timer. Hint: a for loop
-		//for(i = 15; i >= 0 ; i--){
-		//	GPIO_PORTB_DATA_R = LEDPattern[i];
-		//	SysTick_Wait200ms(2);
-		//}
+		SYSCTL_RCGCGPIO_R = PB | PC;	/* Enable PORTB & PORTC clock*/
+		GPIO_PORTB_DIR_R = 0x03;			/* set PORTB pins 0 & 1 as digital OUTPUT pins*/
+		GPIO_PORTB_DEN_R = 0x03;			/* Enable PORTB pins*/
 		
-/*		
-		// after the timed countdown, start the DC motor with the duty cycle to be 80%.
-		while (1){
-					/
+		GPIO_PORTC_DIR_R = 0x80;			/* set PORTC pin4 as INPUT (turning on switch simulating displacement sensor) and 7 as OUTPUT (speaker)*/
+		GPIO_PORTC_DEN_R = 0x90;			/* Enable PORTC pins*/
+						
+		LEDon(); 			/* Turn on the LEDs*/
+		welcome();		/* Display Welcome Message on screen*/
+		SpeakerOn();  /* Play boot up tones on speaker*/	
+		display();		/* Display player name*/
+		
+/*pwrDwn and reset are not activated we play game*/
+		if(pwrDwn() == 0 && reset() == 0){
+			if(gameTimer()< 31){			/* If there is still time left in the game...*/
+					if(shotClock() < 6){	/* If the player still has time to shoot...*/
+						if(goal()== 1){
+							if( PLAYER >= 0 && PLAYER < 2)	/* Check for a vaild player*/
+							goalCounter[PLAYER]++;					/* Update current player's goal counter*/
+							
+						}//if goal
+					}//if shotclock
+			}//if gametimer
+		}//if pwrDwn and reset ==0
+		
+/*pwrDwn not activated but reset IS, we reset the game*/		
+		if(pwrDwn() == 0 && reset() == 1){
+			goalCounter[0] = 0; /* Clear any previously recorded goals*/
+			goalCounter[1] = 0;
+			LEDon(); 			/* Turn on the LEDs*/
+			welcome();		/* Display Welcome Message on screen*/
+			SpeakerOn();  /* Play boot up tones on speaker*/	
+			display();		/* Display player name*/
+			SHOTCLOCK = 0;/* Reset shotclock*/
+			GAMETIMER = 0;/* Reset game timer*/
+			TIMEOUT = 0;	/* Reset the timeout timer*/
+			PLAYER = 0;		/* Reset to player 1*/
+			TURNS = 0;		/* Reset the number of turns taken*/
+		}//if pwrDwn=0 and reset=1
+/*pwrDwn is activated we turn off everything*/
+		if(pwrDwn() == 1){
+			LEDoff();					//Turn off the LEDs
+			goodBye();				//Display goodbye message
+			SpeakerOff();			//Emit goodbye tone
+			
+			return 0;					//Exit main function
 		}
-*/		
+}//main
+
+/*Fucntion to determine state of goals, was one made==1 or no goal==0?*/
+int goal(){
+/* Polling to see if there is a goal, switch activated on PORTC pin4*/
+
+	return 0;							//Exit function
+}
+
+
+/*Function to determine if the game should perform power down protocols*/
+int pwrDwn(){
+	int PwrDwn = 0; /* Is there incomming signal from on/off button?*/
+	return PwrDwn;	/* Power down Protocols hard coded to be deactivated*/
+}
+
+
+/*Function to assess if players want to reset the game*/
+int reset(){
+
+	return 0;							//Exit function
+}
+
+
+/*Function to track the time remaining for a player to take a shot, 5 minute maximum*/
+int shotClock(){
+		int i,j;
+		for(i=0; i<60000; i++){	//About 1 minute 
+			for(j=0;j<3180;j++){ //About 1 millisecond
+			}
+		}
+		SHOTCLOCK++; //Increment the shotclock by 1 minute, this happens when if statement compares the variable						
+	
+		if(SHOTCLOCK == 5){	//Switch to next player's turn
+			switch(PLAYER){
+				case 0: PLAYER = 1;
+								break;
+				case 1: PLAYER = 0;
+								TURNS = 1;	//When we switch back to player 1 there has been 1 turn completed by each player
+								break;
+			}//switch case statement	
+		}//if shotclock == 5	
+		return 0;							//Exit function
+}//END shotclock function
+
+
+
+
+/*Function to track the overall time remaining till game is over, 30 minute maximum*/
+int gameTimer(){
+		if(TURNS < 3){ //we still have time left in the current game
+		
+		}//if
+		return 0;							//Exit function
+}//END game timer function
+
+
+/*Function to automatically turn off the game after 33 minutes, 3 minutes after the end of the game*/
+int ulamaTimeout(){
+	if(TURNS == 3){
+		int i,j;
+		for(i=0; i<60000; i++){	//About 1 minute 
+			for(j=0;j<3180;j++){ //About 1 millisecond
+			}
+		}
+	}//if
+	return 0;							//Exit function
+}
+
+/*Function to display a welcome message*/
+void welcome(){
+
+}			
+
+/*Function to display a goodbye message*/
+void goodBye(){
+
+}
+
+/*Function to display Goal message*/
+void goalScored(){
+
+}
+
+/*Function to display player name*/
+void display(){
+
 }
